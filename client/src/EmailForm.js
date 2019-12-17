aa05c34b9ee1cea7694aebd031a0752a68dcc40b
Doug Fairlie <doug.fairlie@onjcri.org.au>
2019-12-17 11:37:58 +1100

    add alert to email send

--- client/src/EmailForm.js
+++ client/src/EmailForm.js
@@ -24,7 +24,7 @@ handleSubmit = (e) => {
 
     axios
     .post('https://pubswithimpact.herokuapp.com/sendEmail', message)
-    .then(() => console.log("Email sent"))
+    .then(() => alert("Email successfully sent!"))
     .catch(err => {
       console.error(err);
     });
