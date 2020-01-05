6a2ca5597ce29904db74f1a5372d61d9f02266e4
Doug Fairlie <doug.fairlie@onjcri.org.au>
2020-01-05 21:36:15 +1100

    format banner for mobile

--- client/src/SubHeading.js
+++ client/src/SubHeading.js
@@ -2,7 +2,8 @@ import React from 'react'
 
 const SubHeading = () => {
     return (
-    <div className="subBanner">
+    <div className="subBanner" style={{margin: "0 20px"
+}}>
         <p className="subBannerText" style={{lineHeight: 1.3, fontSize: "0.9rem"
         }}>All scientists like to keep abreast of what is being published by their colleagues. <span style={{fontWeight: "bold", fontStyle: "italic", color: "red" }}>"Aussie Pubs with Impact"</span> is an easy way to view the latest biomed papers published by Australian scientists in the most influential journals. The journals we monitor are chosen by our panel of experienced life scientists who feel they are at the top of the field or widely regarded as being important, irrespective of impact factor.</p>
         <hr style={{marginTop: "20px"}}/>
