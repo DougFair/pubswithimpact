a974d5979807e1523052d03e9bef1651e112ab92
Doug Fairlie <doug.fairlie@onjcri.org.au>
2020-01-07 19:51:57 +1100

    format contact page for mobile

--- client/src/EmailForm.js
+++ client/src/EmailForm.js
@@ -43,55 +43,54 @@ render () {
                 <div className="contactHeader">
                     <h1>Contact Us!</h1>
                     <p>Please feel free to contact us about any aspect of this website - we are very open to suggestions on how you think we can improve things! Suggestions for any other features you would like to see added, or specific journal titles you would like included in our standard curated list, are also most welcome - all ideas will be considered. Thank You!</p>
-
                 </div>
 
-
-                <form onSubmit={this.handleSubmit} className="emailForm" style={{marginTop:"50px"}}>
-                    <div>
-                        <label  htmlFor="name">Your Name</label>
-                        <input type="text" 
-                        name="name"
-                        value={this.state.name}
-                        placeholder="You Name"
-                        onChange = {this.handleChange}
-                        className="contactInput"
-                        />
-                    </div>
-                    <div>
-                        <label  htmlFor="from">Your Email Address</label>
-                        <input type="email" 
-                        name="from"
-                        placeholder="e.g. joe.bloggs@email.com"
-                        value={this.state.from}
-                        onChange = {this.handleChange}
-                        className="contactInput"
-                        />
-                    </div>
-                    <div>
-                        <label  htmlFor="subject">Subject</label>
-                        <input type="text"
-                        name="subject"
-                        placeholder="Message subject"
-                        value={this.state.subject}
-                        onChange = {this.handleChange}
-                        className="contactInput"
-                        />
-                    </div>
-                    <div>
-                        <label  htmlFor="text">Message</label>
-                        <textarea type="text"
-                        name="text"
-                        placeholder="Type message here..."
-                        value={this.state.text}
-                        onChange = {this.handleChange}
-                        className="contactInputTextArea"
-                        rows="10"
-                        />
-                    </div>
-                    <button className="contactInputButton">Submit</button>
-
-                </form>
+                <div className="emailForm">
+                    <form onSubmit={this.handleSubmit}>
+                        <div>
+                            <label  htmlFor="name">Your Name</label>
+                            <input type="text" 
+                            name="name"
+                            value={this.state.name}
+                            placeholder="You Name"
+                            onChange = {this.handleChange}
+                            className="contactInput"
+                            />
+                        </div>
+                        <div>
+                            <label  htmlFor="from">Your Email Address</label>
+                            <input type="email" 
+                            name="from"
+                            placeholder="e.g. joe.bloggs@email.com"
+                            value={this.state.from}
+                            onChange = {this.handleChange}
+                            className="contactInput"
+                            />
+                        </div>
+                        <div>
+                            <label  htmlFor="subject">Subject</label>
+                            <input type="text"
+                            name="subject"
+                            placeholder="Message subject"
+                            value={this.state.subject}
+                            onChange = {this.handleChange}
+                            className="contactInput"
+                            />
+                        </div>
+                        <div>
+                            <label  htmlFor="text">Message</label>
+                            <textarea type="text"
+                            name="text"
+                            placeholder="Type message here..."
+                            value={this.state.text}
+                            onChange = {this.handleChange}
+                            className="contactInputTextArea"
+                            rows="10"
+                            />
+                        </div>
+                        <button className="contactInputButton">Submit</button>
+                    </form>
+                </div>
             </div>
         )
     }
