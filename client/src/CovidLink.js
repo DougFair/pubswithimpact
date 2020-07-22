fe48e72e233ac14d0898c5cc426936deb97ef5f3
Doug Fairlie <doug.fairlie@onjcri.org.au>
2020-07-22 14:50:42 +1000

    fix covid link size

--- client/src/CovidLink.js
+++ client/src/CovidLink.js
@@ -5,7 +5,7 @@ const CovidLink = () => {
     return (
         <div className="covidContainer">
         <h3 className="covidPara">NEW: Keep up with the latest COVID-19 papers at: {"\u00A0"}</h3>
-        <h3 className="covidPara"><a href="https://covid19pubs.herokuapp.com/" target="_blank" rel="noopener noreferrer">covid19pubs.herokuapp.com</a></h3>
+        <h3 className="covidPara"><a className="covidTag" href="https://covid19pubs.herokuapp.com/" target="_blank" rel="noopener noreferrer">covid19pubs.herokuapp.com</a></h3>
         </div>
     )
 }
