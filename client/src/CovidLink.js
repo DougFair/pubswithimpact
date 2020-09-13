8431eafbea6b3cdaaa78e2026346b3cdd69c2044
Doug Fairlie <doug.fairlie@onjcri.org.au>
2020-09-13 16:12:01 +1000

    add google analytics tag

--- client/src/CovidLink.js
+++ client/src/CovidLink.js
@@ -4,7 +4,7 @@ import "./CovidLink.css"
 const CovidLink = () => {
     return (
         <div className="covidContainer">
-        <h3 className="covidPara">NEW: Keep up with the latest COVID-19 papers at: {"\u00A0"}</h3>
+        <h3 className="covidPara">Keep up with the latest COVID-19 papers at: {"\u00A0"}</h3>
         <h3 className="covidPara"><a className="covidTag" href="https://covid19pubs.herokuapp.com/" target="_blank" rel="noopener noreferrer">covid19pubs.herokuapp.com</a></h3>
         </div>
     )
